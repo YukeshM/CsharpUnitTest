9e19664d4d78bccd528fc3b0f5083e25ef3c48a7
Yukesh.M <mlyukesh@gmail.com>
2021-08-23 08:50:46 +0530

    msUnitTest

---
+++ calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Calculator
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing, writing + operator instead of * operator.  
+            return num1 + num2;
+        }
+    }
+}
---
+++ calculatorTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTestMethod()
+        {
+            Calculator calculator = new Calculator();
+            double res = calculator.Add(4, 4);
+            Assert.AreEqual(res, 8);
+        }
+
+        [TestMethod()]
+        public void SubstractTestMethod()
+        {
+            Calculator calculator = new Calculator();
+            double res = calculator.Substract(4, 3);
+            Assert.AreEqual(res, 1);
+        }
+
+        [TestMethod()]
+        public void divideTest()
+        {
+            Calculator calculator = new Calculator();
+            double res = calculator.divide(4, 2);
+            Assert.AreEqual(res, 2);
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            double res = calculator.Multiply(5, 6);
+            Assert.AreEqual(res, 30);
+        }
+    }
+}
